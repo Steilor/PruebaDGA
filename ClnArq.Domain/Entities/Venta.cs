843705e42446f85a22b6e23e2493f6f27c447f62
Steilor <steilormateo.44@gmail.com>
2025-06-16 23:53:51 -0400

    Add identity

--- ClnArq.Domain/Entities/Venta.cs
+++ ClnArq.Domain/Entities/Venta.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ClnArq.Domain.Entities;
 
-namespace ClnArq.Domain.Entities
+public class Venta
 {
-    public class Venta
-    {
-        public int Id { get; set; }
-        public DateTime Fecha { get; set; } = DateTime.UtcNow;
-        public decimal Total { get; set; }
+    public int Id { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.UtcNow;
+    public decimal Total { get; set; }
 
-        public Guid ClienteId { get; set; }
-        public Cliente Cliente { get; set; } = null!;
+    public Guid ClienteId { get; set; }
+    public Cliente Cliente { get; set; } = null!;
 
-        public ICollection<DetalleVenta> Detalles { get; set; } = new List<DetalleVenta>();
-    }
+    public ICollection<DetalleVenta> Detalles { get; set; } = new List<DetalleVenta>();
 }
--- ClnArq.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ ClnArq.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using ClnArq.Domain.Repositories;
+using ClnArq.Infrastructure.Identity;
 using ClnArq.Infrastructure.Persistence;
 using ClnArq.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,19 +11,28 @@ namespace ClnArq.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        // 1) Registrar DbContext
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
         services.AddDbContext<ClnArqDbContext>(options =>
-            options.UseSqlServer(
-                configuration.GetConnectionString("DefaultConnection"),
-                sql => sql.MigrationsAssembly(typeof(ClnArqDbContext).Assembly.FullName)
-            )
+            options.UseSqlServer(connectionString)              
         );
 
-      
+        services.AddIdentityApiEndpoints<ApplicationUser>()
+               .AddEntityFrameworkStores<ClnArqDbContext>();
+
+
+        /*services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+        })
+            .AddEntityFrameworkStores<ClnArqDbContext>()
+            .AddDefaultTokenProviders();*/
+
         services.AddScoped<IStoreRepository, StoreRepository>();
 
-        return services;
     }
 }
