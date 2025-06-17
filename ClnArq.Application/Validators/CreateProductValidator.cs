2077fc984995a9b8234d18c690467aa9c1dc7234
Steilor <steilormateo.44@gmail.com>
2025-06-17 14:55:01 -0400

    Product validator

---
+++ ClnArq.Application/Validators/CreateProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using ClnArq.Application.Dtos;
+using FluentValidation;
+
+namespace ClnArq.Application.Validators;
+
+public class CreateProductValidator : AbstractValidator<ProductoDto>
+{
+    public CreateProductValidator()
+    {
+        RuleFor(dto => dto.Nombre)
+             .Length(3, 100);
+
+        RuleFor(dto => dto.Stock)
+           .GreaterThanOrEqualTo(0)
+           .WithMessage("Stock no puede ser un numero negativo.");
+
+
+        RuleFor(dto => dto.Precio)
+              .GreaterThanOrEqualTo(0)
+           .WithMessage("Precio no puede ser un numero negativo.");
+
+    }
+}
---
+++ ClnArq.Domain/Exceptions/ForbidException.cs
@@ -0,0 +1,6 @@
+﻿namespace ClnArq.Domain.Exceptions;
+
+public class ForbidException : Exception
+{
+
+}
