257e5785056fbe4cc0641a5961dfc3d063a44100
Steilor <steilormateo.44@gmail.com>
2025-06-20 19:53:07 -0400

    front resources refactory

--- ClnArq.Domain/Entities/Venta.cs
+++ ClnArq.Domain/Entities/Venta.cs
@@ -7,16 +7,16 @@ public class Venta
     public DateTime Fecha { get; set; } = DateTime.UtcNow;
     public decimal Total { get; set; }
 
-    public Guid ClienteId { get; set; }
-    public Cliente Cliente { get; set; } = null!;
+    public Guid? ClienteId { get; set; }
+    public Cliente? Cliente { get; set; } = null!;
 
-    public Guid ProductoId { get; set; }
-    public Producto Producto { get; set; } = null!;
+    public Guid? ProductoId { get; set; }
+    public Producto? Producto { get; set; } = null!;
 
  
     public string ClienteNombre { get; set; } = string.Empty;
     public string ProductoNombre { get; set; } = string.Empty;
     public decimal ProductoPrecioUnitario { get; set; }
 
-    public ICollection<DetalleVenta> DetallesVenta { get; set; } = new List<DetalleVenta>();
+    public ICollection<DetalleVenta>? DetallesVenta { get; set; } = new List<DetalleVenta>();
 }
---
+++ ClnArq.Infrastructure/Migrations/20250620235113_ventaId2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClnArq.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ventaId2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DetallesVenta_Productos_ProductoId",
+                table: "DetallesVenta");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DetallesVenta_Ventas_VentaId",
+                table: "DetallesVenta");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VentaId",
+                table: "DetallesVenta",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductoId",
+                table: "DetallesVenta",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DetallesVenta_Productos_ProductoId",
+                table: "DetallesVenta",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DetallesVenta_Ventas_VentaId",
+                table: "DetallesVenta",
+                column: "VentaId",
+                principalTable: "Ventas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DetallesVenta_Productos_ProductoId",
+                table: "DetallesVenta");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DetallesVenta_Ventas_VentaId",
+                table: "DetallesVenta");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VentaId",
+                table: "DetallesVenta",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductoId",
+                table: "DetallesVenta",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DetallesVenta_Productos_ProductoId",
+                table: "DetallesVenta",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DetallesVenta_Ventas_VentaId",
+                table: "DetallesVenta",
+                column: "VentaId",
+                principalTable: "Ventas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
